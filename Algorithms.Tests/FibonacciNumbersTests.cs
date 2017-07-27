f04c0e3e60f140096cf49ce0a0bdc8015bffdb06
Alexander <alexyarosh98@gmail.com>
2017-07-27 13:38:40 +0300

    Добавьте файлы проекта.

---
+++ Algorithms.Tests/FibonacciNumbersTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Algorithms.Tests
+{
+    [TestFixture]
+    public class FibonacciNumbersTests
+    {
+        [TestCase(1,new []{1})]
+        [TestCase(2,new []{1,1})]
+        [TestCase(5,new[]{1,1,2,3,5})]
+        [TestCase(15,new[]{ 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610 })]
+        public void FiboGenerate_NumberOfElementsToBeGenerated_IEnumerableObjectOfFibonacciNumbers
+            (int numberOfElements,IEnumerable<int> expectedCollection)
+        {
+            List<int> result = new List<int>(FibonacciNumbers.FiboGenerate(numberOfElements));
+            int index = 0;
+
+            Assert.AreEqual(result.Count(),expectedCollection.Count());
+            foreach (int i in expectedCollection)
+            {
+                Assert.AreEqual(result.ElementAt(index++),i);
+            }
+        }
+
+        [TestCase(-5)]
+        [TestCase(0)]
+        [TestCase(int.MinValue)]
+        public void FiboGenerated_NumberOfElementsLessThenZero_ArgumentOutOfRangeException(int numberOfElements)
+        {
+            var ex = Assert.Catch<ArgumentOutOfRangeException>(() =>
+            {
+                List<int> list=new List<int>(FibonacciNumbers.FiboGenerate(numberOfElements));
+            }
+            );
+
+            StringAssert.Contains($"{nameof(numberOfElements)} must be a positive number", ex.Message);
+        }
+    }
+}
---
+++ Algorithms/FibonacciNumbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class FibonacciNumbers
+    {
+        /// <summary>
+        /// Find specified amount of fibonacci numbers
+        /// </summary>
+        /// <param name="numberOfElements">Number of elements to be found</param>
+        /// <returns>IEnumberable object of fibonacci numbers</returns>
+        public static IEnumerable<int> FiboGenerate(int numberOfElements)
+        {
+            if(numberOfElements<=0) throw new ArgumentOutOfRangeException($"{nameof(numberOfElements)} must be a positive number");
+
+            int firstNum = 1;
+            int secondNum = 1;
+            int sum;
+            int currentIndex = 0;
+
+            yield return firstNum;
+            if (numberOfElements <= 1) yield break;
+            yield return secondNum;
+
+            while (currentIndex < numberOfElements - 2)
+            {
+                yield return sum = firstNum + secondNum;
+                firstNum = secondNum;
+                secondNum = sum;
+                currentIndex++;
+            }
+        }
+    }
+}
